c57bfa7449b0b35e80cc4134da43b057bf9a6a4a
MartinParidon <martinparidon@gmail.com>
2019-01-02 16:49:18 +0100

    Update KalmanFilter.c

--- KalmanFilter.c
+++ KalmanFilter.c
@@ -26,7 +26,7 @@ int main(void)
 	fillMeasurementMatrix(meanX, stddevX, meanY, stddevY, MATRIX_LEN1(measurementsVel), MATRIX_LEN2(measurementsVel), measurementsVel);
 	
 	/* Initial matrix states */
-	float x[4];											/* state vector: x, y, x', y' */
+	float x[4];										/* state vector: x, y, x', y' */
 	float P[4][4];										/* Matrix for covariances betweens variables */
 	float A[4][4];										/* Transition Matrix */
 	float H[2][4];										/* Measurement Fct (How sensor values are converted) */
@@ -59,7 +59,9 @@ int main(void)
 	FILE *f = fopen("out.txt", "w");					/* Define file handle */
 	
 	FILE *gnuplot = popen("gnuplot -persistent", "w");  /* Define gnuplot handle: https://stackoverflow.com/questions/14311640/how-to-plot-data-by-c-program */
-	fprintf(gnuplot, "plot '-'\n");
+	
+	fprintf(gnuplot, "%s \n", "set title \"var\"");	/* Adjust according to the data you're plotting (see end of for loop) */
+        fprintf(gnuplot, "plot '-'\n");
 	
 	for (int iVal = 0; iVal < numVals; iVal++)				/* May be overwritten for debug purposes */
 	{
@@ -106,9 +108,9 @@ int main(void)
 		WRITEFLOATARRAYTOFILETAB(f,x)
 		WRITEENTERTOFILE(f)
 		
-		// fprintf(gnuplot, "%d %g\n", iVal, x[2]);		/* May be overwritten for debug purposes */
-		// fprintf(gnuplot, "%d %g\n", iVal, Z[0]);		/* See and compare to Matlab: Z[0], x[2], P[2][2] */
-		fprintf(gnuplot, "%d %g\n", iVal, P[2][2]);
+		fprintf(gnuplot, "%d %g\n", iVal, x[2]);		/* vEstim */
+		// fprintf(gnuplot, "%d %g\n", iVal, Z[0]);		/* vMeas */
+		// fprintf(gnuplot, "%d %g\n", iVal, P[2][2]);		/* var */
 		
 	}
 	
@@ -136,286 +138,4 @@ void fillMeasurementMatrix(float meanX, float stddevX, float meanY, float stddev
 /************************************* Init matrices ********************************************/
 void initMatrices(float dt, float x[4], float P[4][4], float A[4][4], float H[2][4], float R[2][2], float Q[4][4], float I[4][4])
 {					
-	x[0] = 0;		x[1] = 0;		x[2] = 10;		x[3] = 0;	
-	
-	P[0][0] = 10;	P[0][1] = 0;	P[0][2] = 0;	P[0][3] = 0;	
-	P[1][0] = 0;	P[1][1] = 10;	P[1][2] = 0;	P[1][3] = 0;
-	P[2][0] = 0;	P[2][1] = 0;	P[2][2] = 10;	P[2][3] = 0;	
-	P[3][0] = 0;	P[3][1] = 0;	P[3][2] = 0;	P[3][3] = 10;
-										
-	A[0][0] = 1;	A[0][1] = 0;	A[0][2] = dt;	A[0][3] = 0;	
-	A[1][0] = 0;	A[1][1] = 1;	A[1][2] = 0;	A[1][3] = dt;
-	A[2][0] = 0;	A[2][1] = 0;	A[2][2] = 1;	A[2][3] = 0;	
-	A[3][0] = 0;	A[3][1] = 0;	A[3][2] = 0;	A[3][3] = 1;
-										
-	H[0][0] = 0;	H[0][1] = 0;	H[0][2] = 1;	H[0][3] = 0;	
-	H[1][0] = 0;	H[1][1] = 0;	H[1][2] = 0;	H[1][3] = 1;
-	
-	R[0][0] = 1;	R[0][1] = 0;		
-	R[1][0] = 0;	R[1][1] = 1;
-	
-	Q[0][0] = 0.25 * powf(dt, 4);	Q[0][1] = 0.25 * powf(dt, 4);	Q[0][2] = 0.5 * powf(dt, 3);	Q[0][3] = 0.5 * powf(dt, 3);	
-	Q[1][0] = 0.25 * powf(dt, 4);	Q[1][1] = 0.25 * powf(dt, 4);	Q[1][2] = 0.5 * powf(dt, 3);	Q[1][3] = 0.5 * powf(dt, 3);
-	Q[2][0] = 0.5 * powf(dt, 3);	Q[2][1] = 0.5 * powf(dt, 3);	Q[2][2] = powf(dt, 2);	Q[2][3] = powf(dt, 2);
-	Q[3][0] = 0.5 * powf(dt, 3);	Q[3][1] = 0.5 * powf(dt, 3);	Q[3][2] = powf(dt, 2);	Q[3][3] = powf(dt, 2);
-	
-	I[0][0] = 1;	I[0][1] = 0;	I[0][2] = 0;	I[0][3] = 0;	
-	I[1][0] = 0;	I[1][1] = 1;	I[1][2] = 0;	I[1][3] = 0;
-	I[2][0] = 0;	I[2][1] = 0;	I[2][2] = 1;	I[2][3] = 0;
-	I[3][0] = 0;	I[3][1] = 0;	I[3][2] = 0;	I[3][3] = 1;
-}
-
-/************************************* Random number generator **********************************/
-/* KalmanFilter.c Implements the Polar form of the Box-Muller Transformation
-(c) Copyright 1994, Everett F. Carter Jr.
-    	Permission is granted by the author to use
-	this software for any application provided this
-	copyright notice is preserved.
-*/
-float box_muller(float m, float s)	
-{				        			
-	float x1, x2, w, y1;
-	static float y2;
-	static int use_last = 0;
-
-	if (use_last)		        	
-	{
-		y1 = y2;
-		use_last = 0;
-	}
-	else
-	{
-		do {
-			x1 = 2.0 * (((float)rand()) / ((float)RAND_MAX)) - 1.0;
-			x2 = 2.0 * (((float)rand()) / ((float)RAND_MAX)) - 1.0;
-			w = x1 * x1 + x2 * x2;
-		} while ( w >= 1.0 );
-
-		w = sqrt( (-2.0 * log( w ) ) / w );
-		y1 = x1 * w;
-		y2 = x2 * w;
-		use_last = 1;
-	}
-
-	return( m + y1 * s );
-}
-
-/************************************* Print functions ******************************************/
-void showFloatMatrix(int N, int M, float array[N][M])
-{
-	int n, m;
-	for (n = 0; n < N; n++)
-	{
-		for (m = 0; m < M; m++)
-			printf("%f\t", array[n][m]);
-		printf("\n");
-	}
-}
-
-void showFloatArray(int N, float array[])
-{
-	int n;
-	for (n = 0; n < N; n++)
-	{
-		printf("%f\t", array[n]);
-	}
-	printf("\n");
-}
-
-/************************************* Matrix and vector calculations ***************************/
-void multiplyMatrix_NN_NN_NN(int N, float mat1[N][N], float mat2[N][N], float res[N][N]) 
-{ 
-    int n1, n2, n3; 
-	memset(res, 0, sizeof(float)*N*N);
-    for (n1 = 0; n1 < N; n1++) 
-    { 
-        for (n2 = 0; n2 < N; n2++) 
-        { 
-            for (n3 = 0; n3 < N; n3++) 
-                res[n1][n2] += mat1[n1][n3]*mat2[n3][n2]; 
-        } 
-    } 
-} 
-
-void multiplyMatrix_NM_MN_NN(int N, int M, float mat1[N][M], float mat2[M][N], float res[N][N]) 
-{ 
-    int n1, n2, m; 
-	memset(res, 0, sizeof(float)*N*N);
-    for (n1 = 0; n1 < N; n1++) 
-    { 
-        for (n2 = 0; n2 < N; n2++) 
-        { 
-            for (m = 0; m < M; m++) 
-                res[n1][n2] += mat1[n1][m]*mat2[m][n2]; 
-        } 
-    } 
-} 
-
-void multiplyMatrix_NN_NM_NM(int N, int M, float mat1[N][N], float mat2[N][M], float res[N][M]) 
-{ 
-    int n1, m, n2; 
-	memset(res, 0, sizeof(float)*N*M);
-    for (n1 = 0; n1 < N; n1++) 
-    { 
-        for (m = 0; m < M; m++) 
-        { 
-            for (n2 = 0; n2 < N; n2++) 
-                res[n1][m] += mat1[n1][n2]*mat2[n2][m]; 
-        } 
-    } 
-} 
-
-void multiplyMatrix_MN_NN_MN(int M, int N, float mat1[M][N], float mat2[N][N], float res[M][N]) 
-{ 
-    int m, n1, n2; 
-	memset(res, 0, sizeof(float)*M*N);
-    for (m = 0; m < M; m++) 
-    { 
-        for (n1 = 0; n1 < N; n1++) 
-        { 
-            for (n2 = 0; n2 < N; n2++) 
-                res[m][n1] += mat1[m][n2]*mat2[n2][n1]; 
-        } 
-    } 
-} 
-
-void multiplyMatrixWithVector_NM_M_N(int N, int M, float mat[N][M],  float vec[M], float res[N]) 
-{ 
-	int n, m;
-	float sumProd;
-	for (n = 0; n < N; n++)
-	{
-		sumProd = 0;
-		for (m = 0; m < M; m++)
-		{
-			sumProd += mat[n][m] * vec[m];
-		}
-		res[n] = sumProd;
-	} 
-} 
-
-void transposeMatrix_NM_MN(int N, int M, float mat1[N][M],  float mat2[M][N])
-{
-	int n, m;
-	for (n = 0; n < N; n++)
-	{
-		for (m = 0; m < M; m++)
-			mat2[m][n] = mat1[n][m];
-	}
-}
-
-void subtractVectorFromVector(int N, float vec1[N], float vec2[N], float res[N])
-{
-	int n;
-	for (n = 0; n < N; n++) 
-	{
-		res[n] = vec1[n] - vec2[n];
-	}
-}
-
-void addVectors(int N, float vec1[N], float vec2[N], float res[N])
-{
-	int n;
-	for (n = 0; n < N; n++) 
-	{
-		res[n] = vec1[n] + vec2[n];
-	}
-}
-
-void addMatrices(int N, int M, float mat1[N][M],  float mat2[N][M], float res[N][M])
-{
-	int n, m;
-	for (n = 0; n < N; n++) 
-	{
-		for (m = 0 ; m < M; m++) 
-		{
-			 res[n][m] = mat1[n][m] + mat2[n][m];
-		}
-	}
-}
-
-void subtractMatrixFromMatrix(int N, int M, float mat1[N][M],  float mat2[N][M], float res[N][M])
-{
-	int n, m;
-	for (n = 0; n < N; n++) 
-	{
-		for (m = 0 ; m < M; m++) 
-		{
-			 res[n][m] = mat1[n][m] - mat2[n][m];
-		}
-	}
-}
-
-/************************************* Matrix inverse calculations ******************************/
-void cofactor(float a[2][2],float d[2][2],float n,float determinate){
-	float b[2][2],c[2][2];
-	int l,h,m,k,i,j;
-	for (h=0;h<n;h++)
-		for (l=0;l<n;l++){
-			m=0;
-			k=0;
-			for (i=0;i<n;i++)
-				for (j=0;j<n;j++)
-					if (i != h && j != l){
-						b[m][k]=a[i][j];
-						if (k<(n-2))
-							k++;
-						else{
-							k=0;
-							m++;
-						}
-					}
-			c[h][l] = pow(-1,(h+l))*det(b,(n-1));	/* c = cofactor Matrix */
-		}
-	transpose(c,d,n,determinate);	//* read function */
-}
-
-void inverse(float a[2][2],float d[2][2],int n,float det){
-	if(det == 0)
-		printf("\nInverse of Entered Matrix is not possible\n");
-	else if(n == 1)
-		d[0][0] = 1;
-	else
-		cofactor(a,d,n,det);	/* read function */
-}
-
-float det(float a[2][2],int n){
-	int i;
-	float b[2][2],sum=0;
-	if (n == 1)
-return a[0][0];
-	else if(n == 2)
-return (a[0][0]*a[1][1]-a[0][1]*a[1][0]);
-	else
-		for(i=0;i<n;i++){
-			minor(b,a,i,n);	/* read function */
-			sum = (float) (sum+a[0][i]*pow(-1,i)*det(b,(n-1)));	/* sum = determinate matrix */
-		}
-return sum;
-}
-
-void transpose(float c[2][2],float d[2][2],float n,float det){
-	int i,j;
-	float b[2][2];
-	for (i=0;i<n;i++)
-		for (j=0;j<n;j++)
-			b[i][j] = c[j][i];
-	for (i=0;i<n;i++)
-		for (j=0;j<n;j++)
-			d[i][j] = b[i][j]/det;	/* array d[][] = inverse matrix */
-}
-
-void minor(float b[2][2],float a[2][2],int i,int n){
-	int j,l,h=0,k=0;
-	for(l=1;l<n;l++)
-		for( j=0;j<n;j++){
-			if(j == i)
-				continue;
-			b[h][k] = a[l][j];
-			k++;
-			if(k == (n-1)){
-				h++;
-				k=0;
-			}
-		}
-}
+	x
